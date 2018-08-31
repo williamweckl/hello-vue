670821071f4ea1694fe97ac9c65c1d7087825705
William Weckl <william.weckl@gmail.com>
2018-08-31 13:04:16 -0300

    First component

---
+++ src/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1 :title="msg">{{msg}}</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+  export default	{
+    data () {
+      return {
+        msg: "Eaeeee"
+      }
+		}
+  }
+</script>
